21b4221af2a811ada89f2fe8e5fec3da27fa0a26
Amirhossein Mirshahi <amirmsh@student.ubc.ca>
2023-11-29 16:58:27 -0800

    Add task model and update createTask function

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,13 +1,17 @@
+const { model } = require("mongoose");
+const Task = require("../models/task");
+
 const getAllTasks = (req, res) => {
   res.send("All tasks");
 };
 
-const createTask = (req, res) => {
-  res.json(req.body);
+const createTask = async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json({ task });
 };
 
 const getTask = (req, res) => {
-  res.json({id:req.params.id});
+  res.json({ id: req.params.id });
 };
 
 const updateTask = (req, res) => {
