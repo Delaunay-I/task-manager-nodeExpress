f9f9177a67765f31cff0a17b3135f848da623810
Amirhossein Mirshahi <amirmsh@student.ubc.ca>
2023-11-29 22:37:06 -0800

    Update port to use environment variable

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ require("dotenv").config();
 const notFound = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
-
 // middleware
 app.use(express.static("./public"));
 app.use(express.json());
@@ -16,8 +15,7 @@ app.use("/api/v1/tasks", tasks);
 app.use(notFound);
 app.use(errorHandlerMiddleware);
 
-
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
